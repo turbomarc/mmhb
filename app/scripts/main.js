9c2f294ace9fcd237920e49f0dfac80d44aa41da
turbomarc <marc@cognitect.com>
2016-02-12 20:55:58 -0500

    add online order info

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
 console.log('\'Allo \'Allo!');
+<!--
+function popup(mylink, windowname)
+{
+    console.log('in popup');
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, windowname, 'width=400,height=200,scrollbars=yes');
+return false;
+}
